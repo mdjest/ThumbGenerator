82f35b4f1aa9649a4bf2f87ce4abbe4a71a35fcd
Alexey Kiselev <madjest27@gmail.com>
2016-11-07 16:34:14 +0700

    bugfix

--- src/WideImageThumb.php
+++ src/WideImageThumb.php
@@ -1,85 +1,116 @@
 <?php
 
 class WideImageThumb {
+	
+	// For create self config make thumb.php in config folder (only for Laravel 4)
 
-	static $format = array(
-		'default'  => array(
-			'size' => array(250, 250),
-			'true' => true
+	static $default_config = array(
+		'format'        => array(
+			'default'  => array(
+				'size' => array(250, 250),
+				'true' => true
+			),
+			'listing'  => array(
+				'size' => array(100, 100),
+				'crop' => true
+			),
+			'lightbox' => array(
+				'size' => array(1000, 1000),
+			),
 		),
-		'listing'  => array(
-			'size' => array(100, 100),
-			'crop' => true
+		'defaultFormat' => 'default',
+		'max_size'      => array(
+			'weight' => 8,      // Mb
+			'h'      => 3000,   // Px
+			'w'      => 3000,   // Px
 		),
-		'lightbox' => array(
-			'size' => array(1000, 1000),
-		),
+		'path_cache'    => '/cache',
+		'empty_image'   => '/images/empty_image.png',
 	);
 
-	static private $max_size_Mb = 8;
 	static private $instance = null;
-	static private $defaultFormat = 'default';
 	static private $formatName = null;
 
+	private $original = null;
 	private $url_cache = null;
 
 	static public function factory($original, $format = 'default') {
 		self::$formatName = $format;
-		self::$instance = new self($original, $format);
+		self::$instance   = new self($original, $format);
 
 		return self::$instance;
 	}
 
 	public function __construct($original) {
-		$format = self::$format[self::$formatName];
-		if (!is_file(public_path() . $original)) {
+		$this->original = $original;
+
+		if (!is_file(public_path() . $this->original)) {
 			return $this;
 		}
-		if(filesize(public_path() . $original) > Config::get('app.max_upload_size')){
-			return null;
-		}
-		$cache = '/cache' . dirname($original) . '/' . self::$formatName . '/';
-		if (!is_dir(public_path() . $cache)) {
-			mkdir(public_path() . $cache, 0777, true);
+
+		if (filesize(public_path() . $this->original) > self::config('max_size.weight') * 1024 * 1024) {
+			return $this;
 		}
 
-		$cache_filename = md5($original) . '.' . pathinfo(public_path() . $original, PATHINFO_EXTENSION);
+		$cache_dir = self::config('format.path_cache') . dirname($this->original) . '/' . self::$formatName . '/';
 
-		if (!is_file(public_path() . $cache . $cache_filename)) {
+		if (!is_dir(public_path() . $cache_dir)) {
+			mkdir(public_path() . $cache_dir, 0777, true);
+		}
+
+		$cache_filename = md5($this->original) . '.' . pathinfo(public_path() . $this->original, PATHINFO_EXTENSION);
+		$cache_path     = public_path() . $cache_dir . $cache_filename;
+
+		if (!is_file($cache_path)) {
 			try {
-				$info = getimagesize(public_path() . $original);
-				if($info[0] > 3000 or $info[1] > 3000){
-					return $this;
-				}
-				$image = WideImage::load(public_path() . $original);
-				if (isset($format['crop']) and $format['crop']) {
-									
-					$image = $image->resize($format['size'][0], $format['size'][1], 'outside', 'down')->crop('center', 'center', $format['size'][0], $format['size'][1]);
-
-				} else {
-					$image = $image->resize($format['size'][0], $format['size'][1], 'inside', 'down');
-				}
-				$image->saveToFile(public_path() . $cache . $cache_filename);
+				$this->generate($cache_path);
 			} catch (Exception $e) {
-
+				Log::error('WideImageThumb / error generate Thumb');
 			}
-			//			Mews\Thumb\Facades\Thumb::create(public_path() . $original)->make('resize', $format['size'])->save(public_path() . $cache, $cache_filename);
 		}
 
-		$this->url_cache = $cache . $cache_filename;
+		$this->url_cache = $cache_dir . $cache_filename;
 
 		return $this;
 	}
 
-	private function getUrlCache() {
+	static private function config($key) {
+		if (Config::get('thumb')) {
+			return Config::get('thumb.' . $key);
+		}
+		return array_get(self::$default_config, $key);
+	}
+
+	private function generate($cache_path) {
+		$format = self::config('format.' . self::$formatName);
+
+		$info = getimagesize(public_path() . $this->original);
+
+		if ($info[0] > self::config('max_size.w') or $info[1] > self::config('max_size.h')) {
+			return $this;
+		}
+
+		$image = WideImage\WideImage::load(public_path() . $this->original);
+
+		if (isset($format['crop']) and $format['crop']) {
+			$image = $image->resize($format['size'][0], $format['size'][1], 'outside', 'down')->crop('center', 'center', $format['size'][0], $format['size'][1]);
+		} else {
+			$image = $image->resize($format['size'][0], $format['size'][1], 'inside', 'down');
+		}
+
+		$image->saveToFile($cache_path);
+	}
+
+	public function getUrlCache() {
 		return $this->url_cache;
 	}
 
 	public function get() {
 		$result = $this->getUrlCache();
-		if(!$result){
-			$result = Thumb::factory('/images/empty_image.png', self::$formatName)->get();
+		if (!$result) {
+			$result = self::factory(self::config('empty_image'), self::$formatName)->get();
 		}
+
 		return $result;
 	}
 
